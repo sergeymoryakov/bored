ff0bf759052c436a56ba7d41b0ddc39dca696ac1
Sergey Moryakov <sergey@moryakov.com>
2023-06-16 17:00:18 +0300

    separate script

--- bored.js
+++ bored.js
@@ -1,3 +1,7 @@
+const CHECKED_CLASSNAME = "checked";
+const CHECKED_BTN_VALUE = "Try Again";
+const CHECKED_EMOTION = "Wow, not bored anymore 🔥";
+
 const boredContainerNode = document.getElementById("boredContainer");
 const emotionNode = document.getElementById("emotion");
 const suggestNode = document.getElementById("suggest");
@@ -6,12 +10,20 @@ const findOutBtnNode = document.getElementById("findOutBtn");
 findOutBtnNode.addEventListener("click", renderSuggest);
 
 function renderSuggest() {
-    suggestNode.innerText = getResponse();
+    getResponse()
+        .then((activity) => {
+            suggestNode.innerText = activity;
+            emotionNode.innerText = CHECKED_EMOTION;
+            findOutBtnNode.innerText = CHECKED_BTN_VALUE;
+            boredContainerNode.classList.toggle(CHECKED_CLASSNAME);
+        })
+        .catch((error) => {
+            console.log("Error:", error);
+        });
 }
 
 function getResponse() {
-    fetch("http://www.boredapi.com/api/activity/")
+    return fetch("http://www.boredapi.com/api/activity/")
         .then((response) => response.json())
         .then((res) => res.activity);
-    return res.activity;
 }
